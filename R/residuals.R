ca9648f9597c3f6755f8c6ddc4b64010ee1c7a99
JensWahl <jens.christian.wahl@nr.no>
2020-12-11 14:35:04 +0100

    fix error, add reference to validation paper

--- R/residuals.R
+++ R/residuals.R
@@ -1,10 +1,14 @@
 #' Calculate one-step-ahead (OSA) residuals for stochastic volatility model.
 #' 
+#' This function is very time consuming and by default computes the one-step-ahead residual for the last 100 observations. 
+#' See the function \link[TMB]{oneStepPredict} and the paper in the references for more details. 
+#' 
 #' @param object A \code{stochvolTMB} object.
-#' @oparam conditional Index vector of observations that are fixed during OSA. By default the residuals of the last 100
-#' observations are calculated. 
+#' @param conditional Index vector of observations that are fixed during OSA. By default the residuals of the last 100
+#' observations are calculated. If set to \code{NULL} it will calculate one-step-ahead residuals for all observations. 
 #' @param ... Currently not used. 
 #' @return Vector of one-step-ahead residuals. If the model is correctly specified, these should be standard normal.  
+#' @references \url{https://www.researchgate.net/publication/316581864_Validation_of_ecological_state_space_models_using_the_Laplace_approximation}
 #' @export 
 
 residuals <- function(object, conditional = 1:(object$nobs - 100), ...) {
