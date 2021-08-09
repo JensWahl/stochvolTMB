816c2e82af02f9a6f4b54fc73451a75e634f3d02
JensWahl <jens.christian.wahl@nr.no>
2021-08-09 15:44:05 +0200

    import predict from stats

--- R/stochvolTMB-package.R
+++ R/stochvolTMB-package.R
@@ -1,5 +1,5 @@
 #' @importFrom data.table data.table setnames := as.data.table is.data.table rbindlist setcolorder
-#' 
+#' @importFrom stats predict
 #' @useDynLib stochvolTMB, .registration = TRUE
 #' @keywords internal
 NULL
